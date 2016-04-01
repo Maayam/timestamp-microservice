b3d5b2e6ee263eae4c78f258ceb8bb723fa7f27d
Maayam <antoine.devos.francis@gmail.com>
2016-04-01 13:13:04 +0000

    first commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
 
+var ms = require('./node_modules/timestamp-ms/index.js');
+
 var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
@@ -25,9 +27,29 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-routes(app, passport);
+//routes(app, passport);
 
 var port = process.env.PORT || 8080;
+
+app.get('/:query', function(req, res){
+	console.log('got query');
+	var query = req.params['query'].toLowerCase();
+	if ( ms.isTimestamp(query) ){
+		console.log('timestamp found!');
+		res.send( {"timestamp": query, "natural": ms.toNatural(query)} );
+	}
+	else if ( ms.isNatural(query) ){
+		console.log('natural date found !');
+		res.send( {"timestamp": ms.toTimestamp(query), "natural": query} );
+	}
+	else{
+		console.log('nothing found...');
+		res.send( {"timestamp": null, "natural": null} );
+	}
+}).get('/', function(req, res){
+	console.log('got no query');
+});
+
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
