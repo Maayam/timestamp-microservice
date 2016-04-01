d8b08f16eca513b2902e9260af2e8166df79fd87
Maayam <antoine.devos.francis@gmail.com>
2016-04-01 13:23:47 +0000

    cleaned server.js

--- server.js
+++ server.js
@@ -2,17 +2,12 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
 var session = require('express-session');
 
 var ms = require('./node_modules/timestamp-ms/index.js');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -23,9 +18,6 @@ app.use(session({
 	resave: false,
 	saveUninitialized: true
 }));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 //routes(app, passport);
 
